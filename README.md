1540d7c327eebafadfa2ee944a89e9453081ab48
logan-lieou <logan.lieou@protonmail.com>
2021-01-09 04:45:51 -0600

    program is almost done

---
+++ README.md
@@ -0,0 +1,17 @@
+## Twitter CLI
+
+This application is a command line interface in order to interact with twitter without having to use twitter. This is an extremely simple implementation of this idea and is written in python at the moment, if I decide that the app is not performant enough I will re-write this is go.
+
+![]("./docs/pic1.png")
+
+> what the program currently looks like
+
+## Use
+
+Inside of the config.yaml file put your consumer keys as well as your authentication tokens the program pulls these from the config.yaml file and initializes the api with those tokens.
+
+Make sure that your program has Read and Write access otherwise you will not be able to tweet from the cli.
+
+## TODO
+
+- Abstract iterating through tweets
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def init():
     # read config
     with open(r"./config.yaml") as f:
         contents = yaml.load(f, Loader=yaml.FullLoader)
-        print(contents)
         f.close()
 
     # get keys from config
@@ -13,6 +12,11 @@ def init():
     c_sec = contents['consumer']['sec']
     a_key = contents['token']['key']
     a_sec = contents['token']['sec']
+
+    # if any of the expected contents are missing
+    if (c_key == "" or c_sec == "" or a_key == "" or a_sec == ""):
+        # this creates an error that I can catch yes I know this is bad
+        return broken 
 
     # initialize the api
     api = twitter.Api(consumer_key = c_key,
@@ -26,30 +30,60 @@ def init():
 def get_timeline(api):
     # get timeline
     timeline = api.GetHomeTimeline()
-    # iterate through the tweets
+
+    # output the tweets
     for i in range(len(timeline)):
-        text = timeline[i].text
-        name = timeline[i].user.screen_name
-        print("{0}\n{1}".format(name, text))
+        text = timeline[len(timeline)-1-i].text
+        name = timeline[len(timeline)-1-i].user.screen_name
+        coor = timeline[len(timeline)-1-i].created_at
+        print("\n{0}\n{1}\n{2}\n".format(name, text, coor))
+
+def send_tweet(api):
+    post = str(input("> "))
+    api.PostUpdates(post)
+
+def lookup_tweets(api):
+    search = str(input("Search Term: "))
+    # query string after q= is the search term we're looking for all the other params are less important
+    results = api.GetSearch(raw_query="q={0}%20&result_type=recent&since=2014-07-19&count=100".format(search))
+
+    # output the tweets
+    for i in range(len(results)):
+        text = results[len(results)-1-i].text
+        name = results[len(results)-1-i].user.screen_name
+        coor = results[len(results)-1-i].created_at
+        print("\n{0}\n{1}\n{2}\n".format(name, text, coor))
 
 # main function
 def main():
-    api = init()
-    user_selection = int(input("\nSelect an Option:\n0:\tGet Timeline\n1:\tTweet\n2:\tSearch\n3:\tQuit\n"))
+    # catch the error
+    try:
+        api = init()
+    except NameError:
+        print("Error keys missing from config.yaml")
+        return
+
+    # initialize user selection
+    user_selection = 4
+    # while loop
     while(user_selection != 3):
-        user_selection = int(input("\nSelect an Option:\n0:\tGet Timeline\n1:\tTweet\n2:\tSearch\n3:\tQuit\n"))
+        try:
+            user_selection = int(input("\nSelect an Option:\n0:\tGet Timeline\n1:\tTweet\n2:\tSearch\n3:\tQuit\n"))
+        except ValueError:
+            print("Enter a valid value!")
         # no switch statement in python??
         if (user_selection == 0):
-            print("YOUR TIMELINE:\n")
             get_timeline(api)
         elif (user_selection == 1):
-            pass
+            send_tweet(api)
         elif (user_selection == 2):
-            pass
+            lookup_tweets(api)
         elif (user_selection == 3):
+            # does nothing the loop quits on it's own
             break
         else:
-            print("There was an error!")
+            # for out of bounds such as user_selection = 4
+            print("There was an error!") 
 
 # execute main function
 if __name__ == "__main__":
